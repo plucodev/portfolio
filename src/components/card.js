d0c523f71a375fe2188afdbb1fdf55903ebeffce
plucodev <paololucanodeveloper@gmail.com>
2020-10-25 14:48:34 -0400

    installed bootstrap

---
+++ src/components/card.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Card = props => {
+    return (
+        <div className="card">
+            Card
+        </div>
+    )
+}
+
+Card.propTypes = {
+
+}
+
+export default Card
