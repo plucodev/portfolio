2edd0c754c26fad70c84e827f444b92899dcaf0c
plucodev <paololucanodeveloper@gmail.com>
2020-10-25 16:02:14 -0400

    configured bootstrap

--- src/views/home.js
+++ src/views/home.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Card from '../components/card';
+import Navbar from '../components/navbar';
 
 const Home = props => {
     return (
         <div>
+            <Navbar />
             Home Page
             <Card title={`Fetch`} />
         </div>
